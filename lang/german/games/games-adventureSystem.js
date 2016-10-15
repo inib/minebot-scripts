5c97d4f9f011f7f42b31deb13d1a43da9f733970
inib <alpinehd@gmail.com>
2016-10-15 23:04:38 +0200

    "spaced" emotes in adventure translation

--- lang/german/games/games-adventureSystem.js
+++ lang/german/games/games-adventureSystem.js
@@ -21,13 +21,13 @@ $.lang.register('adventuresystem.top5.empty', 'Derzeit haben wir noch keine Held
 
 $.lang.register('adventuresystem.stories.1.title', 'Roshan Raubzug');
 $.lang.register('adventuresystem.stories.1.chapter.1', 'Ein lautes Gebrüll ertönt im Chat und die schweren Schritte des Untiers sorgen für Erdbeben! Roshan legt den Chat in Schutt und Asche. Jetzt sind unsere Helden gefragt, das Monster zu vernichten!"');
-$.lang.register('adventuresystem.stories.1.chapter.2', 'Verluste?! In einem epischen Kampf gegen Roshan wurden (caught) getötet. WutFace');
+$.lang.register('adventuresystem.stories.1.chapter.2', 'Verluste?! In einem epischen Kampf gegen Roshan wurden (caught) getötet. WutFace ');
 $.lang.register('adventuresystem.stories.1.chapter.3', 'Das Untier ist tot! (survivors) haben überlebt und konnten sich das Aegis of the Immortal sichern!');
 
 $.lang.register('adventuresystem.stories.2.title', 'Rettet Tinyfin martinTinyfin ');
-$.lang.register('adventuresystem.stories.2.chapter.1', 'martinTinyfin : Mrglgl Zu Hilfe Chat! Der grosse, böse FrankerZ ist hinter mir her, mrglgl!');
-$.lang.register('adventuresystem.stories.2.chapter.2', 'FrankerZ : Komm her, kleiner Murloc! Dich gibts heute als Hauptgericht!');
-$.lang.register('adventuresystem.stories.2.chapter.3', '(caught) ist das egal und schauen lieber weiter Martin zu. OSsloth');
+$.lang.register('adventuresystem.stories.2.chapter.1', ' martinTinyfin : Mrglgl Zu Hilfe Chat! Der grosse, böse FrankerZ ist hinter mir her, mrglgl!');
+$.lang.register('adventuresystem.stories.2.chapter.2', ' FrankerZ : Komm her, kleiner Murloc! Dich gibts heute als Hauptgericht!');
+$.lang.register('adventuresystem.stories.2.chapter.3', '(caught) ist das egal und schauen lieber weiter Martin zu. OSsloth ');
 $.lang.register('adventuresystem.stories.2.chapter.4', 'Bis an die Zähne bewaffnet stellen sich (survivors) FrankerZ entgegen und können martinTinyfin befreien!');
 
 $.lang.register('adventuresystem.stories.3.title', 'Spinnen?! WutFace');
@@ -35,23 +35,23 @@ $.lang.register('adventuresystem.stories.3.chapter.1', 'WutFace /\\/\\( * o . o 
 $.lang.register('adventuresystem.stories.3.chapter.2', '(caught) haben versucht die Spinne in ein Glas zu locken und lebend zu fangen FailFish und wurden dabei tödlich gebissen.');
 $.lang.register('adventuresystem.stories.3.chapter.3', 'Doch (survivors) kennen sich mit Spinnen aus, da gibts nur eins: Drauftreten! *matsch*');
 
-$.lang.register('adventuresystem.stories.4.title', 'ChefFrank Kuchen backen');
-$.lang.register('adventuresystem.stories.4.chapter.1', 'NomNom Kuchenzeit! NomNom Der Chat ist hungrig und unser wagemutiges Heldenteam wurde in die Küche geschickt.');
-$.lang.register('adventuresystem.stories.4.chapter.2', 'GivePLZ In dem Kessel mit Strychnin - kochen sie erst Terpentin - darauf ein Narkotikum - einen Schuss Petroleum OpieOP');
-$.lang.register('adventuresystem.stories.4.chapter.3', 'StinkyCheese Ein, zwei Tropfen Jauchensaft - Blut von Fliegen massenhaft - Eingeblasstes Spinnenbein...');
-$.lang.register('adventuresystem.stories.4.chapter.4', '...und in den Kessel fallen rein: (caught). OH NEEEEIN NotLikeThis');
-$.lang.register('adventuresystem.stories.4.chapter.5', '(survivors) habens geschafft! Der Kuchen ist im Ofen und es duftet bereits! DansGame Wer probiert als erstes? panicBasket');
+$.lang.register('adventuresystem.stories.4.title', ' ChefFrank Kuchen backen');
+$.lang.register('adventuresystem.stories.4.chapter.1', ' NomNom Kuchenzeit! NomNom Der Chat ist hungrig und unser wagemutiges Heldenteam wurde in die Küche geschickt.');
+$.lang.register('adventuresystem.stories.4.chapter.2', ' GivePLZ In dem Kessel mit Strychnin - kochen sie erst Terpentin - darauf ein Narkotikum - einen Schuss Petroleum OpieOP ');
+$.lang.register('adventuresystem.stories.4.chapter.3', ' StinkyCheese Ein, zwei Tropfen Jauchensaft - Blut von Fliegen massenhaft - Eingeblasstes Spinnenbein...');
+$.lang.register('adventuresystem.stories.4.chapter.4', '...und in den Kessel fallen rein: (caught). OH NEEEEIN NotLikeThis ');
+$.lang.register('adventuresystem.stories.4.chapter.5', '(survivors) habens geschafft! Der Kuchen ist im Ofen und es duftet bereits! DansGame Wer probiert als erstes? panicBasket ');
 
-$.lang.register('adventuresystem.stories.5.title', 'MrDestructoid Reparaturauftrag');
-$.lang.register('adventuresystem.stories.5.chapter.1', 'DansGame Der Brawl ist mal wieder manipuliert! Schraubenschlüssel und Hammer eingepackt, geht\'s los! VoHiYo');
-$.lang.register('adventuresystem.stories.5.chapter.2', 'MrDestructoid Irgendjemand hat in der Kneipe für die nächste Prügelei Fallen aufgestellt! Wie Unfair!');
-$.lang.register('adventuresystem.stories.5.chapter.3', '*BOOM* - (caught) stürmen in die Kneipe und haben dabei die erste Sprengfalle direkt "gefunden". EleGiggle');
-$.lang.register('adventuresystem.stories.5.chapter.4', 'RitzMitz - (survivors) sind vorsichtiger und können mehrere grosse Bärenfallen sicher entschärfen, der nächste Brawl ist gerettet! KappaRoss');
+$.lang.register('adventuresystem.stories.5.title', ' MrDestructoid Reparaturauftrag');
+$.lang.register('adventuresystem.stories.5.chapter.1', ' DansGame Der Brawl ist mal wieder manipuliert! Schraubenschlüssel und Hammer eingepackt, geht\'s los! VoHiYo ');
+$.lang.register('adventuresystem.stories.5.chapter.2', ' MrDestructoid Irgendjemand hat in der Kneipe für die nächste Prügelei Fallen aufgestellt! Wie Unfair!');
+$.lang.register('adventuresystem.stories.5.chapter.3', '*BOOM* - (caught) stürmen in die Kneipe und haben dabei die erste Sprengfalle direkt "gefunden". EleGiggle ');
+$.lang.register('adventuresystem.stories.5.chapter.4', ' RitzMitz - (survivors) sind vorsichtiger und können mehrere grosse Bärenfallen sicher entschärfen, der nächste Brawl ist gerettet! KappaRoss');
 
 $.lang.register('adventuresystem.stories.6.title', 'Auf zur Pokemon Jagd!');
 $.lang.register('adventuresystem.stories.6.chapter.1', 'Martin hat im Stadtpark ein Relaxo lokalisiert PogChamp - Doch was ist das?');
-$.lang.register('adventuresystem.stories.6.chapter.2', 'Eine Horde Kids hat den Spawn auch schon entdeckt! Los Helden vertriebt die Knirpse! 4Head');
-$.lang.register('adventuresystem.stories.6.chapter.3', 'SwiftRage ATTACK... ne doch nicht - (caught) bekommen es mit der Angst zu tun und laufen davon. FailFish');
+$.lang.register('adventuresystem.stories.6.chapter.2', 'Eine Horde Kids hat den Spawn auch schon entdeckt! Los Helden vertriebt die Knirpse! 4Head ');
+$.lang.register('adventuresystem.stories.6.chapter.3', 'SwiftRage ATTACK... ne doch nicht - (caught) bekommen es mit der Angst zu tun und laufen davon. FailFish ');
 $.lang.register('adventuresystem.stories.6.chapter.4', '(survivors) haben ausreichend Wasserbomben eingepackt! *SWOSCH* Heulend und triefnass suchend die Kids das Weite BabyRage - Das Relaxo ist unser!');
 
 /*
