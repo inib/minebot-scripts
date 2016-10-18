912f892b4369ad953513762245be788786e00e79
inib <alpinehd@gmail.com>
2016-10-18 17:52:16 +0200

    update 2.3

--- core/updates.js
+++ core/updates.js
@@ -1,12 +1,15 @@
 /**
  * updater.js
  *
- * Update database
+ * Update PhantomBot database
  *
  * This module will be executed before loading any of the other scripts even the core!
  * Add a new wrapped function if you want to apply updates for a new version
  */
 
+/**
+ * PhantomBot v2.0
+ */
 (function() {
     var modules,
         versions,
@@ -15,7 +18,7 @@
 
     /** New setup */
     if ($.changed == true && $.changed != null && $.changed != undefined && !$.inidb.exists('updates', 'installedNewBot') && $.inidb.get('updates', 'installedNewBot') != 'true') {
-        $.consoleLn('Initializing m1nebot Version ' + $.version + ' For The First Time...');
+        $.consoleLn('Initializing PhantomBot Version ' + $.version + ' For The First Time...');
 
         modules = [
         './commands/topCommand.js',
@@ -109,8 +112,16 @@
             $.inidb.set('modules', modules[i], 'false');
         }
 
+        $.consoleLn('Adding new default custom commands...');
+        $.inidb.set('command', 'uptime', '(@sender) (channelname) has been online for (uptime)');
+        $.inidb.set('command', 'followage', '(followage)');
+        $.inidb.set('command', 'playtime', '(@sender) (channelname) has been playing (game) for (playtime)');
+        $.inidb.set('command', 'title', '(@sender) (titleinfo)');
+        $.inidb.set('command', 'game', '(@sender) (gameinfo)');
+        $.inidb.set('command', 'age', '(age)');
+
         $.consoleLn('Installing old updates...');
-        versions = ['installedv2', 'installedv2.0.5', 'installedv2.0.6', 'installedv2.0.7', 'installedv2.0.7.2', 'installedv2.0.8', 'installedv2.0.9', 'installedv2.1.0', 'installedv2.1.1'];
+        versions = ['installedv2', 'installedv2.0.5', 'installedv2.0.6', 'installedv2.0.7', 'installedv2.0.7.2', 'installedv2.0.8', 'installedv2.0.9', 'installedv2.1.0', 'installedv2.1.1', 'installedv2.2.1', 'installedv2.3s', 'installedv2.3.3s'];
         for (i in versions) {
             $.inidb.set('updates', versions[i], 'true');
         }
@@ -125,7 +136,7 @@
 
     /** Version 2.0 updates */
     if (!$.inidb.exists('updates', 'installedv2') || $.inidb.get('updates', 'installedv2') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.0 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0 updates...');
         var tableNamesList = $.inidb.GetFileList(),
             commandsBackup,
             timeBackup,
@@ -186,7 +197,7 @@
             $.inidb.set('modules', defaultDisabledModules[i], 'false');
         }
 
-        $.consoleLn('m1nebot v2.0 updates completed!');
+        $.consoleLn('PhantomBot v2.0 updates completed!');
         $.inidb.set('updates', 'installedv2', 'true');
     }
 
@@ -200,7 +211,7 @@
             './commands/highlightCommand.js',
         ]; //ADD NEW MODULES IN 2.0.5 TO BE DISABLED PLEASE.
 
-        $.consoleLn('Starting m1nebot version 2.0.5 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0.5 updates...');
 
         $.consoleLn('Disabling new default modules...');
         for (i in newDefaultDisabledModules) {
@@ -210,25 +221,25 @@
         $.consoleLn('Removing commandCooldown table...');
         $.inidb.RemoveFile('commandCooldown');
 
-        $.consoleLn('m1nebot v2.0.5 updates completed!');
+        $.consoleLn('PhantomBot v2.0.5 updates completed!');
         $.inidb.set('updates', 'installedv2.0.5', 'true');
     }
 
     if (!$.inidb.exists('updates', 'installedv2.0.6') || $.inidb.get('updates', 'installedv2.0.6') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.0.6 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0.6 updates...');
 
 
         if ($.inidb.exists('chatModerator', 'capsLimit')) {
             $.inidb.del('chatModerator', 'capsLimit');
         }
 
-        $.consoleLn('m1nebot v2.0.6 updates completed!');
+        $.consoleLn('PhantomBot v2.0.6 updates completed!');
         $.inidb.set('updates', 'installedv2.0.6', 'true');
     }
 
     /** Version 2.0.7 updates */
     if (!$.inidb.exists('updates', 'installedv2.0.7') || $.inidb.get('updates', 'installedv2.0.7') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.0.7 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0.7 updates...');
 
         var newDefaultDisabledModules = [
             './handlers/gameWispHandler.js',
@@ -267,13 +278,13 @@
             $.inidb.del('command', 'uptime');
         }
 
-        $.consoleLn('m1nebot v2.0.7 updates completed!');
+        $.consoleLn('PhantomBot v2.0.7 updates completed!');
         $.inidb.set('updates', 'installedv2.0.7', 'true');
     }
 
     /** Version 2.0.7.2 updates */
     if (!$.inidb.exists('updates', 'installedv2.0.7.2') || $.inidb.get('updates', 'installedv2.0.7.2') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.0.7.2 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0.7.2 updates...');
 
         if ($.inidb.exists('chatModerator', 'longMessageMessage')) {
             if ($.inidb.get('chatModerator', 'longMessageMessage').equalsIgnoreCase('false')) {
@@ -281,13 +292,13 @@
             }
         }
 
-        $.consoleLn('m1nebot v2.0.7.2 updates completed!');
+        $.consoleLn('PhantomBot v2.0.7.2 updates completed!');
         $.inidb.set('updates', 'installedv2.0.7.2', 'true');
     }
 
     /** Version 2.0.8 updates */
     if (!$.inidb.exists('updates', 'installedv2.0.8') || $.inidb.get('updates', 'installedv2.0.8') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.0.8 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0.8 updates...');
 
         var newDefaultDisabledModules = [
             './handlers/twitterHandler.js',
@@ -300,24 +311,24 @@
             $.inidb.set('modules', newDefaultDisabledModules[i], 'false');
         }
 
-        $.consoleLn('m1nebot v2.0.8 updates completed!');
+        $.consoleLn('PhantomBot v2.0.8 updates completed!');
         $.inidb.set('updates', 'installedv2.0.8', 'true');
     }
 
     /** Version 2.0.9 updates */
     if (!$.inidb.exists('updates', 'installedv2.0.9') || $.inidb.get('updates', 'installedv2.0.9') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.0.9 updates...');
+        $.consoleLn('Starting PhantomBot version 2.0.9 updates...');
 
         $.consoleLn('Deleting old emotes cache...');
         $.inidb.del('emotescache', 'emotes');
 
-        $.consoleLn('m1nebot v2.0.9 updates completed!');
+        $.consoleLn('PhantomBot v2.0.9 updates completed!');
         $.inidb.set('updates', 'installedv2.0.9', 'true');
     }
 
     /** Version 2.1/2.0.10 updates */
     if (!$.inidb.exists('updates', 'installedv2.1.0') || $.inidb.get('updates', 'installedv2.1.0') != 'true') {
-        $.consoleLn('Starting m1nebot version 2.1 updates...');
+        $.consoleLn('Starting PhantomBot version 2.1 updates...');
 
         $.consoleLn('Aliasing !permission to !group...');
         $.inidb.set('aliases', 'group', 'permission');
@@ -383,20 +394,105 @@
         }
         sounds = "";
 
-        $.consoleLn($.version + ' updates completed!');
+        $.consoleLn('PhantomBot v2.1 updates completed!');
         $.inidb.set('updates', 'installedv2.1.0', 'true');
         $.inidb.set('updates', 'installedNewBot', 'true');//If bot login is deleted after updates were installed we don't want to reset the modules.
     }
 
-    /** Version 2.1.1 updates */
+    /** Version 2.2 updates */
     if (!$.inidb.exists('updates', 'installedv2.1.1') || $.inidb.get('updates', 'installedv2.1.1') != 'true') {
-        $.consoleLn('Starting ' + $.version + ' updates...');
+        $.consoleLn('Starting PhantomBot v2.2 updates...');
 
         $.consoleLn('Disabling new modules...');
         $.inidb.set('modules', './handlers/streamTipHandler.js', 'false');
 
-        $.consoleLn($.version + ' updates completed!');
+        $.consoleLn('PhantomBot v2.2 updates completed!');
         $.inidb.set('updates', 'installedv2.1.1', 'true');
+    }
+
+    /** Version 2.3 updates */
+    if (!$.inidb.exists('updates', 'installedv2.3s') || $.inidb.get('updates', 'installedv2.3s') != 'true') {
+        $.consoleLn('Starting 2.3 updates...');
+
+        $.consoleLn('Disabling new modules...');
+        $.inidb.set('modules', './handlers/bitsHandler.js', 'false');
+        $.inidb.set('modules', './systems/autoHostSystem.js', 'false');
+
+        $.consoleLn('Setting up new default custom commands...');
+        if (!$.inidb.exists('command', 'uptime')) {
+            $.inidb.set('command', 'uptime', '(@sender) (channelname) has been online for (uptime)');
+        }
+        if (!$.inidb.exists('command', 'followage')) {
+            $.inidb.set('command', 'followage', '(followage)');
+        }
+        if (!$.inidb.exists('command', 'playtime')) {
+            $.inidb.set('command', 'playtime', '(@sender) (channelname) has been playing (game) for (playtime)');
+        }
+        if (!$.inidb.exists('command', 'title')) {
+            $.inidb.set('command', 'title', '(@sender) (titleinfo)');
+        }
+        if (!$.inidb.exists('command', 'game')) {
+            $.inidb.set('command', 'game', '(@sender) (gameinfo)');
+        }
+        if (!$.inidb.exists('command', 'age')) {
+            $.inidb.set('command', 'age', '(age)');
+        }
+        if ($.inidb.exists('permcom', 'game set')) {
+            $.inidb.set('permcom', 'setgame', $.inidb.get('permcom', 'game set'));
+        }
+        if ($.inidb.exists('permcom', 'title set')) {
+            $.inidb.set('permcom', 'settitle', $.inidb.get('permcom', 'title set'));
+        }
+
+        $.inidb.del('permcom', 'game set');
+        $.inidb.del('permcom', 'title set');
+
+        $.consoleLn('Updating auto hosting settings...');
+        if ($.inidb.exists('autohost_config', 'force') && $.inidb.get('autohost_config', 'force') == true) {
+            $.inidb.set('autohost_config', 'force', false);
+        }
+        if ($.inidb.exists('autohost_config', 'host_time_minutes') && $.inidb.get('autohost_config', 'host_time_minutes') < 30) {
+            $.inidb.set('autohost_config', 'host_time_minutes', 0);
+        }
+
+        $.consoleLn('Setting up new toggles...');
+        $.inidb.set('adventureSettings', 'warningMessage', true);
+        $.inidb.set('adventureSettings', 'enterMessage', true);
+
+        $.consoleLn('v2.3 updates completed!');
+        $.inidb.set('updates', 'installedv2.3s', 'true');
+    }
+
+    /* version 2.3.3s updates */
+    if (!$.inidb.exists('updates', 'installedv2.3.3s') || $.inidb.get('updates', 'installedv2.3.3s') != 'true') {
+        $.consoleLn('Starting ' + $.version + ' updates...');
+
+        $.consoleLn('Deleting the old emotes cache.');
+        $.inidb.RemoveFile('emotecache');
+
+        $.consoleLn('Updating raffle settings...');
+        if ($.inidb.exists('settings', 'raffleMSGToggle')) {
+            $.inidb.set('raffleSettings', 'raffleMSGToggle', $.inidb.get('settings', 'raffleMSGToggle'));
+            $.inidb.del('settings', 'raffleMSGToggle');
+        }
+
+        if ($.inidb.exists('settings', 'noRepickSame')) {
+            $.inidb.set('raffleSettings', 'noRepickSame', $.inidb.get('settings', 'noRepickSame'));
+            $.inidb.del('settings', 'noRepickSame');
+        }
+
+        if ($.inidb.exists('settings', 'raffleMessage')) {
+            $.inidb.set('raffleSettings', 'raffleMessage', $.inidb.get('settings', 'raffleMessage'));
+            $.inidb.del('settings', 'raffleMessage');
+        }
+
+        if ($.inidb.exists('settings', 'raffleMessageInterval')) {
+            $.inidb.set('raffleSettings', 'raffleMessageInterval', $.inidb.get('settings', 'raffleMessageInterval'));
+            $.inidb.del('settings', 'raffleMessageInterval');
+        }
+
+        $.consoleLn( $.version + ' updates completed!');
+        $.inidb.set('updates', 'installedv2.3.3s', 'true');
     }
 
     /**
